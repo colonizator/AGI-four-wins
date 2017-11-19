5bfdf7321ec4969a8c59c158540715d077b74b23
colonizator <mkmpoli@gmail.com>
2017-11-19 16:07:55 +0100

    Logic (Vertikalle Überprüfung) + Test - fertig Man muss noch weitere Überprüfungen in der Richtung Horisontal überlegen.

--- src/Spiel/LogicImplementierung.java
+++ src/Spiel/LogicImplementierung.java
@@ -1,36 +1,74 @@
 package Spiel;
 
 import static Spiel.Result.*;
-import static Spiel.Chip.*;
 
-public class LogicImplementierung implements Logic{
+public class LogicImplementierung implements Logic {
 
-	int spielfeld[][];
-		
+	Chip spielfeld[][];
+	Result ergebnis;
+
 	public LogicImplementierung() {
-		spielfeld = new int[6][7];
-		
-		spielfeld[0][0] = 1;
-		spielfeld[1][0] = 1;
-		spielfeld[2][0] = 1;
-		spielfeld[3][0] = 1;
-		
-		
+		spielfeld = new Chip[6][7];
 	}
 
-	
+	public boolean istZeileGefuelt() {
+		int zeile = 0;
+		while (zeile < 7) {
+			if (spielfeld[0][zeile] == null) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public Result ergebnisPruefen(Chip chip, int spalte) {
+
+		if (istZeileGefuelt())
+			return UNENTSCHIEDEN;
+
+		for (int i = spielfeld.length - 1; i >= 0; i--) {
+			if (spielfeld[i][spalte] == chip) {
+				if (i >= spielfeld.length - 3) {
+					if (spielfeld[i - 1][spalte] == chip && spielfeld[i - 2][spalte] == chip
+							&& spielfeld[i - 3][spalte] == chip) {
+						return GEWINEN;
+					}
+				}
+			}
+		}
+
+		return NICHTFERTIG;
+	}
+
 	@Override
 	public Result throwChip(Chip chip, int spalte) {
 
-		if(spielfeld[0][0] == spielfeld[1][0]
-		&& spielfeld[1][0] == spielfeld[2][0] 
-		&& spielfeld[2][0] == spielfeld[3][0]){
-			
-			return GEWINEN;
-			
+		for (int i = spielfeld.length - 1; i >= 0; i--) {
+			if (spielfeld[i][spalte] == null) {
+				spielfeld[i][spalte] = chip;
+				i = -1;
+			}
 		}
 		
-		return Result.NICHTFERTIG;
+		ergebnis =  ergebnisPruefen(chip, spalte);
+		
+		if (ergebnis == GEWINEN) {
+			return ergebnis;
+		} else if (ergebnis == NICHTFERTIG) {
+			return ergebnis;
+		} else {
+			return UNENTSCHIEDEN;
+		}
+
+		/*
+		 * if(spielfeld[0][0] == spielfeld[1][0] & spielfeld[0][0] ==
+		 * spielfeld[2][0] & spielfeld[0][0] == spielfeld[3][0]){
+		 * 
+		 * return GEWINEN;
+		 * 
+		 * } else { return NICHTFERTIG; }
+		 */
+
 	}
 
 }
--- src/Spiel/SpielTest.java
+++ src/Spiel/SpielTest.java
@@ -1,25 +1,33 @@
 package Spiel;
+
 import static Spiel.Result.*;
 import static Spiel.Chip.*;
 import static org.junit.Assert.*;
 
-
 import org.junit.Test;
 
-
 public class SpielTest {
-	
-	private Logic logic = new LogicImplementierung();
+
+	private LogicImplementierung logic = new LogicImplementierung();
 
 	@Test
-	public void horizontalLinksUnten() {
-	
-		
-		assertEquals(GEWINEN, logic.throwChip(ROT, 7));
-	//	assertEquals(NICHTFERTIG, logic.throwChip(ROT, 1));
-	//	assertEquals(NICHTFERTIG, logic.throwChip(ROT, 2));
-	//	assertEquals(GEWINEN, logic.throwChip(ROT, 3));
-		
+	public void vertikallLinksUntenGewin() {
+
+		assertEquals(NICHTFERTIG, logic.throwChip(ROT, 0));
+		assertEquals(NICHTFERTIG, logic.throwChip(ROT, 0));
+		assertEquals(NICHTFERTIG, logic.throwChip(ROT, 0));
+		assertEquals(GEWINEN, logic.throwChip(ROT, 0));
+
+	}
+
+	@Test
+	public void vertikalLinksUntenGewin2() {
+
+		assertEquals(NICHTFERTIG, logic.throwChip(ROT, 2));
+		assertEquals(NICHTFERTIG, logic.throwChip(ROT, 2));
+		assertEquals(NICHTFERTIG, logic.throwChip(ROT, 2));
+		assertEquals(GEWINEN, logic.throwChip(ROT, 2));
+
 	}
 
 }
